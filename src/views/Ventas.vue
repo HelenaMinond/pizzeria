b5e9a02938de93a1d5c6229096380a172dbdad0f
Helena Santana <helena.minond@gmail.com>
2021-05-08 19:36:03 -0400

    desarrollo de vista Ventas

--- src/views/Ventas.vue
+++ src/views/Ventas.vue
@@ -1,12 +1,61 @@
 <template>
-  <div class="text-center">
-    <h1 class="mt-3">Soy Ventas</h1>
+  <div class="container">
+
+    <!-- Titulo -->
+    <div class="row">
+      <div class="col-12">
+        <h1>Ventas de Productos</h1>
+      </div>
+    </div>
+    <!-- Final titulo -->
+
+    <!-- Tabla -->
+    <div class="row">
+      <div class="col-12">
+        <table class="table">
+          <!-- Titulares tabla -->
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">ID</th>
+              <th scope="col">Nombre</th>
+              <th scope="col">Precio</th>
+              <th scope="col">Cantidad Vendida</th>
+            </tr>
+          </thead>
+          <!-- Contenido tabla cuando NO hay inventario -->
+          <tbody v-if="ventas && ventas.length === 0">
+            <tr>
+              <td colspan="5">Aún no hay ventas realizadas</td>
+            </tr>
+          </tbody>
+          <!-- Contenido tabla cuando SI hay inventario -->
+          <tbody v-else>
+            <!-- Contenido tabla -->
+            <tr v-for="(val, i) in ventas" :key="i">
+              <th scope="row">{{ i + 1 }}</th>
+              <td>{{ val.id }}</td>
+              <td>{{ val.nombre }}</td>
+              <td>{{ val.precioSubtotal }}</td>
+              <td>{{ val.cantidadVendida }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <!-- Final tabla -->
+    
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
   name: "Ventas",
+  computed: {
+    ...mapState(["ventas"]),
+  },
 }
 </script>
 
