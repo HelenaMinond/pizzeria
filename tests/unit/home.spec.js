11fb42366ff3801c9b26f794f616d2222ab954f1
Helena Santana <helena.minond@gmail.com>
2021-05-09 20:54:03 -0400

    ultimos cambios a test unit

--- tests/unit/home.spec.js
+++ tests/unit/home.spec.js
@@ -2,12 +2,15 @@ import { shallowMount } from "@vue/test-utils";
 import Home from "@/views/Home.vue";
 
 describe("Home.vue", () => {
+
+  //Test para evaluar presencia de texto
   test("Presencia de texto en etiqueta h1", () => {
     const wrapper = shallowMount(Home);
     const h1 = wrapper.find("h1");
     expect(h1.text()).toContain("Frontendnini");
     })
 
+  //Test para evaluar presencia de clase
   test("Presencia de clase titular", () => {
     const wrapper = shallowMount(Home);
     const titular = wrapper.find(".titular");
